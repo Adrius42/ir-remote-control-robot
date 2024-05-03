f537139943acc1cd39a3fb908099351e2f00dafa
Adrius42 <15740180+Adrius42@users.noreply.github.com>
2024-05-03 18:03:39 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -22,6 +22,26 @@ function car_right () {
     mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 50)
     mecanumRobotV2.Motor(LR.Lower_right, MD.Back, 50)
 }
+}
+function Set_Up_IR () {
+    IR_UP = 70
+    IR_OK = 64
+    IR_Right = 67
+    IR_Left = 68
+    IR_Down = 21
+    IR_0 = 82
+    IR_1 = 22
+    IR_2 = 25
+    IR_3 = 13
+    IR_4 = 12
+    IR_5 = 24
+    IR_6 = 94
+    IR_7 = 8
+    IR_8 = 28
+    IR_9 = 90
+    IR_ = 66
+    IR_10 = 74
+}
 irRemote.connectInfrared(DigitalPin.P0)
 let val = 0
 let val2 = 0
